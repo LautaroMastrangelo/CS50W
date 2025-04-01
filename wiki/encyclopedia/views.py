d53c4f8e741e152cfa3bb68de704405fbef02624
Lautaro Mastrangelo <mastrangelolautaro@gmail.com>
2025-04-01 20:50:11 -0300

    finished entry_page and index_page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
-
+from django.http import HttpResponse
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry = util.get_entry(title)
+    if entry is None:
+        return render(request, "encyclopedia/error.html")
+    return render(request, "encyclopedia/entry.html", 
+        {"title":title, "content":markdown2.markdown(entry)})
